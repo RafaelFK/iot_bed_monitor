6ba785c15f912713ef44d7d18e304d6732be1a25
Rafael F. Katopodis <rafaelkatopodis@gmail.com>
2018-12-08 15:12:38 -0200

    Realizando POSTs

--- Firmware/micro_firmware/micro_firmware.ino
+++ Firmware/micro_firmware/micro_firmware.ino
@@ -1,10 +1,3 @@
-/**
-   BasicHTTPClient.ino
-
-    Created on: 24.05.2015
-
-*/
-
 #include <Arduino.h>
 
 #include <ESP8266WiFi.h>
@@ -12,22 +5,19 @@
 
 #include <ESP8266HTTPClient.h>
 
-#define USE_SERIAL Serial
-
 ESP8266WiFiMulti WiFiMulti;
 
 void setup() {
 
-  USE_SERIAL.begin(115200);
-  // USE_SERIAL.setDebugOutput(true);
+  Serial.begin(115200);
 
-  USE_SERIAL.println();
-  USE_SERIAL.println();
-  USE_SERIAL.println();
+  Serial.println();
+  Serial.println();
+  Serial.println();
 
   for (uint8_t t = 4; t > 0; t--) {
-    USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
-    USE_SERIAL.flush();
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
     delay(1000);
   }
 
@@ -42,27 +32,26 @@ void loop() {
 
     HTTPClient http;
 
-    USE_SERIAL.print("[HTTP] begin...\n");
-    // configure traged server and url
-    //http.begin("https://192.168.1.12/test.html", "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
-    http.begin("http://rkatopodis.pythonanywhere.com//"); //HTTP
-
-    USE_SERIAL.print("[HTTP] GET...\n");
+    Serial.print("[HTTP] begin...\n");
+    http.begin("http://rkatopodis.pythonanywhere.com/"); //HTTP
+    http.addHeader("Content-Type", "application/json");
+    Serial.print("[HTTP] POST...\n");
+    
     // start connection and send HTTP header
-    int httpCode = http.GET();
+    int httpCode = http.POST("{\"name\": \"Rafael\"}");
 
     // httpCode will be negative on error
     if (httpCode > 0) {
       // HTTP header has been send and Server response header has been handled
-      USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+      Serial.printf("[HTTP] POST... code: %d\n", httpCode);
 
       // file found at server
       if (httpCode == HTTP_CODE_OK) {
         String payload = http.getString();
-        USE_SERIAL.println(payload);
+        Serial.println(payload);
       }
     } else {
-      USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
 
     http.end();
