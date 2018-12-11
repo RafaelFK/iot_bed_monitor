dcbe7121c0fbd403a2a53ea1f418b1cf7b104611
Rafael F. Katopodis <rafaelkatopodis@gmail.com>
2018-12-11 04:38:15 -0200

    Calculando centro de massa e o exibindo

--- WebServer/iot.py
+++ WebServer/iot.py
@@ -1,17 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 from datetime import datetime
+import json
 
 app = Flask(__name__)
 
 # Configuração do banco de dados
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="rkatopodis",
-    password="Rlrf3606",
-    hostname="rkatopodis.mysql.pythonanywhere-services.com",
-    databasename="rkatopodis$bed_monitor",
-)
+with open('config.json') as config:
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+        username=config["db_user"],
+        password=config["db_pass"],
+        hostname=config["db_host"],
+        databasename=config["db_name"],
+    )
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -33,6 +35,26 @@ class SensorReading(db.Model):
     value_32 = db.Column(db.Float)
 
 # Rotas e handlers
+@app.route('/')
+def index():
+    return render_template('index.html', readings=SensorReading.query.order_by(db.desc(SensorReading.time)).limit(10).all())
+
+
+@app.route('/latest')
+def latest():
+    # return db.session.query(SensorReading).order_by(db.desc(SensorReading.id)).first()
+    reading = SensorReading.query.order_by(SensorReading.id.desc()).first()
+
+    return jsonify({
+        'timestamp': reading.time,
+        'value_11': reading.value_11,
+        'value_12': reading.value_12,
+        'value_21': reading.value_21,
+        'value_22': reading.value_22,
+        'value_31': reading.value_31,
+        'value_32': reading.value_32
+    })
+
 @app.route('/report', methods=['POST'])
 def report():
     # Registrando leitura no banco
@@ -50,5 +72,3 @@ def report():
     db.session.commit()
 
     return "Received!"
-
-    
